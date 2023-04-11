444365292f2fca49722118186a43d7bc87268ea8
Camily Gimenes <camilygimenes2@gmail.com>
2023-04-11 11:32:36 -0300

    Variaveis e inputs

---
+++ Variaveis_e_input/app.js
@@ -0,0 +1,13 @@
+const readline = require('readline').createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const minhaPrimeiraConstanteString = 'Minha primeira constante';
+console.log(minhaPrimeiraConstanteString);
+
+let leituraDeCampo ;
+readline.question('Por favor digite sua idade:', input => {
+    leituraDeCampo = input;
+    console.log(`o usuario digitou: ${leituraDeCampo}`);
+})
